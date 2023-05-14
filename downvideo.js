c3003c04934907c806e95887acc17c2d3d49b987
VietLH <vietlh@Viet-Cat---MacBook-Pro.local>
2023-05-14 18:21:06 +0700

    update for fb

--- downvideo.js
+++ downvideo.js
@@ -2,98 +2,143 @@
 // ==UserScript==
 // @name         Get Facebook Video Link
 // @namespace    http://tampermonkey.net/
-// @version      0.1.2
+// @version      0.1.3
 // @description  Simply get the downloadable video facebooklink from the page source
 // @author       Viet Cat
 // @match        https://www.facebook.com/*
 // @grant        none
 // ==/UserScript==
+
+var findPlayVideoInterval;
+var videoDownloadPanel;
+var videoLinksPanel;
+var infoPanel;
+var rawVideoLinks = [];
+var hasStarted = false;
+let topOffset;
+let leftOffset;
+let videoDownloadPanelId = "pnlVidDnl";
+let videoLinksPanelId = "pnlVidLk";
+let infoPanelID = "pnlInfo";
+
 (function () {
   // Wait until the page fully loaded
   window.addEventListener("load", function () {
-    var findPlayVideoInterval = setInterval(generateLinksPanel, 1000);
+    findPlayVideoInterval = setInterval(checkForLinks, 2000);
   });
 })();
 
-function generateLinksPanel(){
-    // Find a div with aria-label="Play Video"
-    var playButton = document.querySelector('video[playsinline]');
-
-    //check if the panel for all link is existed
-    var videoLinksPanel = document.getElementById('pnlVidLk');
-    // check if the panel for all buttons is existed
-    var emptyText = document.getElementById('txtPromt');
-    //check if the panel for all buttons is existed
-    var buttonLinksPanel = document.getElementById('pnlButLk');
-    if(videoLinksPanel == null){
-        //create a new panel for all links
-        videoLinksPanel = document.createElement("div");
-        videoLinksPanel.name = "pnlVideoLinks";
-        videoLinksPanel.id = "pnlVidLk";
-        videoLinksPanel.style.position = "fixed";
-        videoLinksPanel.style.top = "50px";
-        videoLinksPanel.style.left = "50px";
-        videoLinksPanel.style.width = "fit-content";
-        videoLinksPanel.style.height = "fit-content";
-        videoLinksPanel.style.zIndex = "5000";
-        videoLinksPanel.style.backgroundColor = "yellow";
-         // Add the panel to the DOM
-        document.body.appendChild(videoLinksPanel);
-  }else{
-    //Action for link panel
-    }
-    if(emptyText == null){
-        //create a promt text
-        emptyText = document.createElement("p");
-        emptyText.id = "txtPromt";
-        //Add promt text to the link panel
-        emptyText.innerHTML = "Searching for videos...";
-        videoLinksPanel.appendChild(emptyText);
+function checkForLinks(){
+    appendLogText("scanning....");
+    if(!hasStarted){
+        hasStarted = true;
+        initComponents();
+        appendLogText("Remove all links...");
+        rawVideoLinks = [];
+        appendURL();
+        hasStarted = false;
     }else{
-    //Action for link panel
-    }
-    if(buttonLinksPanel == null){
-        //create a new panel for all links
-        buttonLinksPanel = document.createElement("div");
-        buttonLinksPanel.name = "pnlButtonLinks";
-        buttonLinksPanel.id = "pnlButLk";
-        buttonLinksPanel.style.position = "fixed";
-        buttonLinksPanel.style.width = "fit-content";
-        buttonLinksPanel.style.height = "fit-content";
-        buttonLinksPanel.style.zIndex = "5000";
-        buttonLinksPanel.style.backgroundColor = "yellow";
-         // Add the panel to the DOM
-        videoLinksPanel.appendChild(buttonLinksPanel);
-  }else{
-    //Action for link panel
-    }
-    if (playButton) {
-        console.log("Play Video found!");
-        emptyText.innerHTML = "Play Video found!";
-        //remove all old elements
-        if(buttonLinksPanel != null){
-        removeAllChildNodes(buttonLinksPanel);
-      }else{
-        //Action for link panel
-      }
-      //Append URL to button
-      appendURL(buttonLinksPanel);
-    }else{
-      console.log("No Play Video found!!!");
-        emptyText.innerHTML = "No Play Video found!!!";
-      if(buttonLinksPanel != null){
-        removeAllChildNodes(buttonLinksPanel);
-    }else{
-      //Action for link panel
-    }
+        appendLogText("Scanning is in progress already...");
     }
 }
 
-function appendURL(videoLinksPanel){
+function generateVideoLinksPanel(){
+    appendLogText("Generating Video Links panel...");
+    //create a new panel for all links
+    videoLinksPanel = document.createElement("div");
+    videoLinksPanel.name = videoLinksPanelId;
+    videoLinksPanel.id = videoLinksPanelId;
+    videoLinksPanel.style.width = 'fit-content';
+    videoLinksPanel.style.height = 'fit-content';
+    videoLinksPanel.style.position = 'relative';
+    videoLinksPanel.style.margin = '10px 0px';
+    videoLinksPanel.style.zIndex = '5000';
+    videoLinksPanel.style.display = 'flex';
+    videoLinksPanel.style.flexDirection = 'column';
+    videoLinksPanel.style.alignItems = 'center';
+    videoLinksPanel.style.backgroundColor = 'yellow';
+
+    // Add the links panel to the download panel
+    videoDownloadPanel.appendChild(videoLinksPanel);
+}
+
+function generateInfoPanel(){
+    appendLogText("Generating Info panel...");
+    //create a new panel for info
+    infoPanel = document.createElement("textarea");
+    infoPanel.name = infoPanelID;
+    infoPanel.id = infoPanelID;
+    infoPanel.style.width = "fit-content";
+    infoPanel.style.height = "fit-content";
+    infoPanel.style.position = 'relative';
+    infoPanel.style.margin = '10px 0px';
+    infoPanel.rows = 4;
+    infoPanel.cols = 30;
+    infoPanel.style.zIndex = "5000";
+    infoPanel.style.backgroundColor = "green";
+
+    // Add the links panel to the download panel
+    videoDownloadPanel.appendChild(infoPanel);
+}
+
+function generateVideoDownloadPanel(){
+    appendLogText("Generating Video Download panel...");
+    //create a new panel for video download
+    videoDownloadPanel = document.createElement("div");
+    videoDownloadPanel.name = videoDownloadPanelId;
+    videoDownloadPanel.id = videoDownloadPanelId;
+    videoDownloadPanel.style.top = "50px";
+    videoDownloadPanel.style.left = "50px";
+    videoDownloadPanel.style.position = "fixed";
+    videoDownloadPanel.style.margin = "10px 0px";
+    videoDownloadPanel.style.width = "fit-content";
+    videoDownloadPanel.style.height = "fit-content";
+    videoDownloadPanel.style.zIndex = "5000";
+    videoDownloadPanel.style.display = "flex";
+    videoDownloadPanel.style.flexDirection = "column";
+    videoDownloadPanel.style.alignItems = 'center';
+    // Add the panel to the DOM
+    document.body.appendChild(videoDownloadPanel);
+
+    // Set the initial position of the floating div
+    topOffset = videoDownloadPanel.offsetTop;
+    leftOffset = videoDownloadPanel.offsetLeft;
+    // Call the updateFloatingTablePosition function on page load and scroll events
+    window.addEventListener('load', updateFloatingTablePosition);
+    window.addEventListener('scroll', updateFloatingTablePosition);
+}
+
+function initComponents(){
+    //check if the panel for video download is existed
+    videoDownloadPanel = document.getElementById(videoDownloadPanelId);
+    if(videoDownloadPanel == null){
+        generateVideoDownloadPanel();
+    }else{
+        removeAllChildNodes(videoDownloadPanel);
+    }
+
+    //check if the panel for all link is existed
+    videoLinksPanel = document.getElementById(videoLinksPanelId);
+    if(videoLinksPanel == null){
+        generateVideoLinksPanel();
+    }else{
+        removeAllChildNodes(videoLinksPanel);
+    }
+
+    //check if the panel for info is existed
+    infoPanel = document.getElementById(infoPanelID);
+    if(infoPanel == null){
+        generateInfoPanel();
+    }else{
+        removeAllChildNodes(infoPanel);
+    }
+}
+
+function appendURL(){
+    appendLogText("Start scanning for links.....");
+    appendLogText("Run a check for HD.....");
     // Get the page source of the current page
     let pageSource = document.documentElement.outerHTML;
-    //get the promt text
-    let prmText = document.getElementById('txtPromt');
     // Use regex to find string between "playable_url_quality_hd":" and ","spherical_video_fallback_urls"
     let regex =
       /playable_url_quality_hd":"(.*?)","spherical_video_fallback_urls/g; //don't forget "g" param to avoid infinite loop
@@ -101,59 +146,59 @@ function appendURL(videoLinksPanel){
     let buttonCounter = 0;
     while(result = regex.exec(pageSource)) {
         // Do something with result[0].
-        prmText.innerHTML = "HD video found. Link added";
+        appendLogText("HD video found. Link added");
         let videoLink = result[1];
         buttonCounter++;
-        console.log("Current counter: " + buttonCounter);
-        console.log("result found : "+ videoLink + " at index: " + result.index);
-        generateLinkButtons(videoLinksPanel, buttonCounter, videoLink)
+        appendLogText("Current counter: " + buttonCounter + "\n\tResult found : "+ videoLink + " \nat index: " + result.index);
+        rawVideoLinks.push(videoLink);
+        //Create the link to the raw video
+        generateLinkButtons("HD", videoLinksPanel, buttonCounter, videoLink);
     }
-    if (result == null){
-        console.log("No HD video found. Running another check...");
-      prmText.innerHTML = "No HD video found. Running another check...";
-        let regex = /playable_url":"(.*?)","playable_url_quality_hd/g; //don't forget "g" param to avoid infinite loop
-        buttonCounter = 0;
-        while(result = regex.exec(pageSource)) {
-            // Do something with result[0].
-            prmText.innerHTML = "SD video found. Link added";
-            let videoLink = result[1];
-            buttonCounter++;
-            console.log("Current counter: " + buttonCounter);
-            console.log("result found : "+ videoLink + " at index: " + result.index);
-            generateLinkButtons(videoLinksPanel, buttonCounter, videoLink)
-        }
-    }else{
-        console.log("HD video found. Link added");
-        prmText.innerHTML = "HD video found. Link added";
+    appendLogText("Running another check for SD...");
+    regex = /playable_url":"(.*?)","playable_url_quality_hd/g; //don't forget "g" param to avoid infinite loop
+    while(result = regex.exec(pageSource)) {
+        // Do something with result[0].
+        appendLogText("SD video found. Link added");
+        let videoLink = result[1];
+        buttonCounter++;
+        appendLogText("Current counter: " + buttonCounter + "\n\tResult found : "+ videoLink + " \nat index: " + result.index);
+        rawVideoLinks.push(videoLink);
+        //Create the link to the raw video
+        generateLinkButtons("SD", videoLinksPanel, buttonCounter, videoLink);
+    }
+
+    if(rawVideoLinks.length > 1){
+        clearInterval(findPlayVideoInterval);
     }
 }
 
-function generateLinkButtons(videoLinksPanel, buttonCounter, videoLink){
+function generateLinkButtons(quality, videoLinksPanel, buttonCounter, videoLink){
+    appendLogText(quality + " Generating link " + buttonCounter + ": " + videoLink);
     //check if the get link button is existed
     var getLinksButton = document.getElementById('dwnVidBtn'+buttonCounter);
     if(getLinksButton == null){
         // Create a new button
-      getLinksButton = document.createElement("button");
+        getLinksButton = document.createElement("button");
 
-      getLinksButton.innerHTML = "Get Video Link "+ buttonCounter;
-       // Resize the button
+        getLinksButton.innerHTML = quality + " - Get Video Link "+ buttonCounter;
+        // Resize the button
         getLinksButton.name = "downVidBtn";
         getLinksButton.id = "dwnVidBtn"+buttonCounter;
-        getLinksButton.style.position = "fixed";
+        getLinksButton.style.position = "relative";
         getLinksButton.style.fontSize = "16px";
         getLinksButton.style.backgroundColor = "green";
         getLinksButton.style.color = "white";
         getLinksButton.style.fontStyle = "bold";
         //Add the button to the panel
         videoLinksPanel.appendChild(getLinksButton);
-        videoLinksPanel.appendChild(document.createElement("br")); //adding linebreak to avoid buttons to overlap each other
-        videoLinksPanel.appendChild(document.createElement("br")); //adding linebreak to avoid buttons to overlap each other
+        //videoLinksPanel.appendChild(document.createElement("br")); //adding linebreak to avoid buttons to overlap each other
+        //videoLinksPanel.appendChild(document.createElement("br")); //adding linebreak to avoid buttons to overlap each other
     }
     //update the link for reference
     getLinksButton.setAttribute('link',videoLink);
     // Click on the button will trigger the function
     getLinksButton.addEventListener("click", function () {
-      console.log("Get Video Link button clicked");
+      appendLogText("Get Video Link button clicked");
       let vidLink = extractURL(this.getAttribute('link'));
   });
 }
@@ -173,7 +218,35 @@ function generateLinkButtons(videoLinksPanel, buttonCounter, videoLink){
 }
 
 function removeAllChildNodes(parentNode){
-  while (parentNode.firstChild) {
-    parentNode.removeChild(parentNode.lastChild);
-  }
+    appendLogText("Removing child nodes....");
+    while (parentNode.firstChild) {
+        if(parentNode.firstChild.firstChild){
+            removeAllChildNodes(parentNode.firstChild.firstChild);
+        }
+        parentNode.removeChild(parentNode.lastChild);
+    }
+}
+
+// Function to update the position of the floating div
+function updateFloatingTablePosition() {
+    if (videoDownloadPanel.getBoundingClientRect().top > topOffset) {
+        videoDownloadPanel.style.top = (videoDownloadPanel.getBoundingClientRect().top - topOffset) + 'px';
+        appendLogText("distance to the top: " +videoDownloadPanel.getBoundingClientRect().top);
+    } else {
+        videoDownloadPanel.style.top = topOffset;
+    }
+    if (videoDownloadPanel.getBoundingClientRect().left > leftOffset) {
+        videoDownloadPanel.style.left = (videoDownloadPanel.getBoundingClientRect().left - leftOffset) + 'px';
+    } else {
+        videoDownloadPanel.style.left = leftOffset;
+    }
+}
+
+function appendLogText(newLog){
+    var logText = "";
+    if(infoPanel != null){
+        logText = infoPanel.value
+        infoPanel.innerHTML = logText + "\n" + newLog;
+    }
+    console.log(newLog + "\n" + logText);
 }
